19ba161798b7d402ed1018d0ce6a8c8b0350208f
JiangShuuu <globelex65@gmail.com>
2023-06-05 16:47:09 +0800

    upd

--- pages/flowbite.vue
+++ pages/flowbite.vue
@@ -19,7 +19,7 @@
     >
       <div class="relative w-full max-w-2xl max-h-full">
         <!-- Modal content -->
-        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+        <div class="relative bg-red rounded-lg shadow dark:bg-gray-700">
           <!-- Modal header -->
           <div
             class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600"
@@ -89,6 +89,90 @@
         </div>
       </div>
     </div>
+
+    <!-- Show tooltip on top -->
+    <button
+      data-tooltip-target="tooltip-top"
+      data-tooltip-placement="top"
+      type="button"
+      class="mb-2 md:mb-0 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+    >
+      Tooltip top
+    </button>
+    <div
+      id="tooltip-top"
+      role="tooltip"
+      class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+    >
+      Tooltip on top
+      <div class="tooltip-arrow" data-popper-arrow></div>
+    </div>
+
+    <!-- Show tooltip on right -->
+    <button
+      data-tooltip-target="tooltip-right"
+      data-tooltip-placement="right"
+      type="button"
+      class="mb-2 md:mb-0 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+    >
+      Tooltip right
+    </button>
+    <div
+      id="tooltip-right"
+      role="tooltip"
+      class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+    >
+      Tooltip on right
+      <div class="tooltip-arrow" data-popper-arrow></div>
+    </div>
+
+    <!-- Show tooltip on bottom -->
+    <button
+      data-tooltip-target="tooltip-bottom"
+      data-tooltip-placement="bottom"
+      type="button"
+      class="mb-2 md:mb-0 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+    >
+      Tooltip bottom
+    </button>
+    <div
+      id="tooltip-bottom"
+      role="tooltip"
+      class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+    >
+      Tooltip on bottom
+      <div class="tooltip-arrow" data-popper-arrow></div>
+    </div>
+
+    <!-- Show tooltip on left -->
+    <button
+      data-tooltip-target="tooltip-left"
+      data-tooltip-placement="left"
+      type="button"
+      class="mb-2 md:mb-0 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+    >
+      Tooltip left
+    </button>
+    <div
+      id="tooltip-left"
+      role="tooltip"
+      class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white bg-red rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+    >
+      Tooltip on left
+      <div class="tooltip-arrow" data-popper-arrow></div>
+    </div>
+
+    <div id="toast-success" class="flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
+    <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200">
+        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
+        <span class="sr-only">Check icon</span>
+    </div>
+    <div class="ml-3 text-sm font-normal">Item moved successfully.</div>
+    <button type="button" class="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-success" aria-label="Close">
+        <span class="sr-only">Close</span>
+        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+    </button>
+</div>
   </div>
 </template>
 
@@ -99,6 +183,6 @@ export default {
   name: 'Scss',
   mounted() {
     initFlowbite()
-  }
+  },
 }
 </script>
